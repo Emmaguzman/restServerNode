d675c956f3f539dc0c9183790ab2667d8538c5db
Emmanuel <emmanuel-a-guzman@hotmail.com>
2020-04-03 20:26:46 -0300

    agregando put post y delete

--- server/server.js
+++ server/server.js
@@ -13,6 +13,27 @@ app.get('/cliente', (req, res) => {
     res.json('getUsuario')
 });
 
+app.post('/cliente', function(req, res) {
+    let body = req.body;
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: "El nombre es necesario"
+        });
+    } else {
+        res.json({ cliente: body });
+    }
+});
+
+app.put('/cliente/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({ id });
+});
+app.delete('/cliente', function(req, res) {
+    res.json('deleteUsuario');
+});
+
+
 app.listen(process.env.PORT, () => {
     console.log('escuchando el puerto', process.env.PORT);
-})
+});
