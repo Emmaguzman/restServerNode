9b6aff3626d48a832e425bedcffa2a4ddcce74fe
Emmanuel <emmanuel-a-guzman@hotmail.com>
2020-04-07 14:39:58 -0300

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlBD;
 if (process.env.NODE_ENV === 'dev') {
     urlBD = "mongodb://localhost:27017/cafe"
 } else {
-    urlBD = "mongodb+srv://el_emma:jQXKeo275ZWKSU2P@cluster0-mldom.mongodb.net/cafe";
+    urlBD = MONGO_URI;
 }
 process.env.URLDB = urlBD;
